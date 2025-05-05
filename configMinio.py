da955407dbf48a72d7828fef413c69c3ab0d34ea
João Gabriel G.S <102331350+KeyLivan@users.noreply.github.com>
2025-05-05 11:22:00 -0300

    Update configMinio.py

--- configMinio.py
+++ configMinio.py
@@ -1,1 +1,6 @@
+#!/usr/bin/env python3.6
 
+# configMinio.py
+MINIO_ACCESS_KEY = "sua_access_key"
+MINIO_SECRET_KEY = "sua_secret_key"
+MINIO_ENDPOINT = "localhost:9000"  # Ou o endereço do seu servidor MinIO
