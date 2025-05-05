52d34ad13a9790f5bdb23ec3e573d50de7ff58af
root <root@freepbx.sangoma.local>
2025-05-05 14:29:54 +0000

    Adicionando configMinio.py ao .gitignore

--- configMinio.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3.6
-
-# configMinio.py
-MINIO_ACCESS_KEY = "sua_access_key"
-MINIO_SECRET_KEY = "sua_secret_key"
-MINIO_ENDPOINT = "localhost:9000"  # Ou o endereço do seu servidor MinIO
