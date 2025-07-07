7ebfb661122b4d7d4f54be9a6954542a540fa62f
João Gabriel G.S <102331350+KeyLivan@users.noreply.github.com>
2025-07-07 09:26:30 -0300

    Update form_transfer.py

--- form_transfer.py
+++ form_transfer.py
@@ -14,7 +14,7 @@ form_type = sys.argv[5]
 finish = sys.argv[6]
 
 # Configurações fixas
-url = f"http://localhost:8000/formulario/{chatvoice_id}"  # Ajuste conforme o seu endpoint real
+url = f"http://3.82.106.88:8000/form-transfer/{chatvoice_id}"  # Ajuste conforme o seu endpoint real
 audio_path = f"/tmp/response_{call_id}_{form_type}.wav"   # Exemplo de onde está o arquivo
 finish = "false"    # ou "true"
 
